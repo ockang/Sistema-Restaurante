46a39566a3960b163fb5370ae030f18f77caebee
violigon <vinicius@oligon.com.br>
2021-09-27 20:58:59 -0300

    fix(app): deprecated code removed

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -13,7 +12,7 @@ var mongodb = require('mongodb');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var expressSession = require('express-session');
-var LocalStrategy = require('passport-local').Strategy;
+
 
 var printer = require('node-thermal-printer');
 
@@ -103,8 +102,6 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
